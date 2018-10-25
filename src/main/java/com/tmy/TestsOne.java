d0980f56b94464b3561c118b813224d533b05716
taominyao <19870311taoMIN>
2018-10-25 22:06:05 +0800

    修改测试类代码（填写版本注释信息）

--- src/main/java/com/tmy/TestsOne.java
+++ src/main/java/com/tmy/TestsOne.java
@@ -5,5 +5,6 @@ public class TestsOne {
     public static void main(String[] args) {
         System.out.printf("hello word!");
         System.out.printf("hello myself！");
+        System.out.printf("hello myself！");
     }
 }
